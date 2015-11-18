f8ec6c57b1c55dfa5ffa7a627c5458c35028d8fd
Noah Kettler <nmkettler@hotmail.com>
2015-11-17 22:13:57 -0800

    first fsx commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Bootstrap 3</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href = "css/bootstrap.min.css" rel = "stylesheet">
+                <link href = "css/style.css" rel = "stylesheet">
+        </head>
+        <body>
+ 
+                <div class = "navbar navbar-inverse navbar-fixed-top">
+                        <div class = "container">
+                               
+                                <a href="#" class="pull-left"><img src="css/fsxLogo.png" height="60" width="80"></a>
+                               
+                                <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+                                        <span class = "icon-bar"></span>
+                                        <span class = "icon-bar"></span>
+                                        <span class = "icon-bar"></span>
+                                </button>
+                               
+                                <div class = "collapse navbar-collapse navHeaderCollapse">
+                               
+                                        <ul class = "nav navbar-nav navbar-right">
+                                       
+                                                <li class = "active"><a href = "#">Home</a></li>
+                                                <li><a href = "#">Competitive Comparison</a></li>
+                                                <li class = "dropdown">
+                                                       
+                                                        <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">Products <b class = "caret"></b></a>
+                                                        <ul class = "dropdown-menu">
+                                                                <li><a href = "#">Trap Blaster</a></li>
+                                                                <li><a href = "#">Soot Sucker</a></li>
+                                                                <li><a href = "#">Competition</a></li>
+                                                                <li><a href = "#">Diesel Filters</a></li>
+                                                        </ul>
+                                               
+                                                </li>
+                                                <li><a href = "#">About Us</a></li>
+                                                <li><a href = "#">Contact</a></li>
+                                       
+                                        </ul>
+                               
+                                </div>
+                               
+                        </div>
+                </div>
+ 				<div class = "widewrapper main">
+ 					<img src = "cleaning1.png" class = "img-responsive" alt = "Responsive image">
+ 					<div class = "jumbotron">
+	 						<h2>FSX DPF CLEANING EQUIPMENT</h1>
+		 						<p class = "text-center">Our DPF cleaners and test bench 
+		 							were proven in our own cleaning operations long before they were
+									released to market. We make sure that buyers of FSX DPF cleaning 
+									equipment have the same machines
+									that are utilized in on our own shops. 
+									The FSX Diesel Particulate Filter cleaning equipment equates to quality.
+								</p>
+								<h2 class = "">CLEANER DPF = LONGER FILTER LIFE</h1>
+		 						<p class = "text-center">FSX manufactures and distributes diesel particulate filter cleaning 
+		 							equipment used in the trucking, transit, off-road, railroad and power generation. 
+		 							Utilizing FSX DPF cleaners assures that the user will get the maximum 
+		 							life possible between cleanings, plus maximize the overall life of the DPF or DOC.
+		 						</p>
+	 					</div>
+	 				<div class = "jumbotron">
+	 						<h2 class = "">PROVEN CLEANING METHOD</h2>
+	 						<p class = "text-center">The machines were designed to duplicate the exact cleaning method 
+	 							that we found worked the best and designed into a professional, compact package.
+	 						</p>
+	 						<h2 class = "">DURABLE MACHINES</h2>
+	 						<p class = "text-center">FSX proves DPF cleaning techniques and transforms the technique 
+	 							into durable machinery that makes cleaning filters easy and reliable for our customers 
+	 							at economical prices.
+	 						</p>
+	 						<a class = "btn btn-default">What Now?</a>
+	 						<a class = "btn btn-default">What Now?</a>
+	 					</div>
+
+	 				<div class = "container">
+	 					
+
+	 				</div>
+ 				</div>
+
+ 				<div class = "navbar navbar-inverse navbar-fixed-bottom">
+
+ 					<div class = "container">
+ 						<p class = "navbar-text pull-left">Site built by Noah</p>
+ 						<a href ="#" class = "navbar-btn btn-danger btn pull-right">Subscribe on Youtube</a>
+ 					</div>
+
+ 				</div>
+                <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script src = "js/bootstrap.js"></script>
+               
+        </body>
+</html>
