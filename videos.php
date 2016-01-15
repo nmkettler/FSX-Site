9eb93d8ec2181be0f31109e8fa15a86836af30f8
Noah Kettler <nmkettler@gmail.com>
2016-01-15 10:49:23 -0800

    heroku automatic deploy test

--- videos.php
+++ videos.php
@@ -2,6 +2,7 @@
 
 <div class = "container">
 	<div class = "jumbotron">
+		<h1 class="text-center">Videos</h1>
 
 		<div class="embed-responsive embed-responsive-16by9">
 		    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0PVSz-F2x6A"></iframe>
